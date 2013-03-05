a5cdfa739eb77f3e74c40bb6b5fe5f69900cea58
Joao Vitor P. Moraes <joao.moraes@lactec.org.br>
2013-03-05 08:13:35 -0300

    Updated install script to only run as root.

--- install_monodevelop.sh
+++ install_monodevelop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source mono-env.sh
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 function die(){
  echo $1
  exit 1
